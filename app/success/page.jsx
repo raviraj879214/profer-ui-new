389c0d4db877586bce2727c49bb4d4acd6d71c02
raviraj879214 <ravirajalphainfo@gmail.com>
2025-07-26 18:30:28 +0530

    Shreyas (#1)
    Co-authored-by: Shreyas OP <shreyasmagajikondi@gmail.com>

--- app/success/page.jsx
+++ app/success/page.jsx
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 
 export default async function SuccessPage({ searchParams }) {
   const { payment_intent: paymentIntentId } = searchParams;
-  //Changed by 12345
+  //Changed by 123456789
   if (!paymentIntentId) redirect("/");
 
   const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);

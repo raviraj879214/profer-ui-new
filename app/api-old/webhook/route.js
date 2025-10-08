a1d75af43a6f0462306f476737e2068e18d6c9fb
raviraj879214 <ravirajalphainfo@gmail.com>
2025-10-08 13:23:03 +0530

    upload api old webhook

--- app/api-old/webhook/route.js
+++ app/api-old/webhook/route.js
@@ -4,21 +4,21 @@ import { headers } from "next/headers";
 import { stripe } from "../../../lib/stripe";
 
 export async function POST(req) {
-  let event;
-  try {
-    event = stripe.webhooks.constructEvent(
-      await req.text(),
-      (await headers()).get("stripe-signature"),
-      process.env.STRIPE_WEBHOOK_SECRET
-    );
-  } catch (err) {
-    return NextResponse.json({ message: `Webhook Error: ${err.message}` }, { status: 400 });
-  }
+  // let event;
+  // try {
+  //   event = stripe.webhooks.constructEvent(
+  //     await req.text(),
+  //     (await headers()).get("stripe-signature"),
+  //     process.env.STRIPE_WEBHOOK_SECRET
+  //   );
+  // } catch (err) {
+  //   return NextResponse.json({ message: `Webhook Error: ${err.message}` }, { status: 400 });
+  // }
 
-  if (event.type === "payment_intent.succeeded") {
-    const data = event.data.object;
-    console.log(`Payment succeeded for: ${data.id}`);
-  }
+  // if (event.type === "payment_intent.succeeded") {
+  //   const data = event.data.object;
+  //   console.log(`Payment succeeded for: ${data.id}`);
+  // }
 
-  return NextResponse.json({ received: true }, { status: 200 });
+  // return NextResponse.json({ received: true }, { status: 200 });
 }
